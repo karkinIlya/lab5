902b67438109bc2f3787d3032a452a30ff77e9cc
Ilya Karkin <karkinia@yandex.ru>
2020-11-29 16:11:53 +0300

    Scripted auto-commit on change (2020-11-29 16:11:53) by gitwatch.sh

--- src/main/java/akkaHttp/AkkaHttpServer.java
+++ src/main/java/akkaHttp/AkkaHttpServer.java
@@ -1,3 +1,21 @@
 public class AddaHttpServer {
-    public void main(String[] args)
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = 1;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress
+                RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+    }
 }
